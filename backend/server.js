7321247b68fbea473359f244931816c292b63980
Alex Truong <87406255+alextruonglt@users.noreply.github.com>
2022-10-07 11:43:34 -0700

    cleaned up server code

--- backend/server.js
+++ backend/server.js
@@ -15,11 +15,7 @@ const app = express()
 app.use(express.json())
 app.use(express.urlencoded({extended: false}))
 
-app.get("/", (req,res) =>{
-    res.status(200).json({
-        message: "welcome to the ticket API"
-    })
-})
+
 
 //Routes
 app.use("/api/users", require("./routes/userRoutes"))
